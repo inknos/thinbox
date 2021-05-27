f93f3363c1fc949c7369e05d93c3eca7f83a95e1
Nicola Sella <nsella@redhat.com>
2021-05-27 16:50:27 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+from distutils.core import setup
+
+
+setup(
+    name='thinbox_py',
+    version='1.0',
+    description='Thinbox rewrittein in pyhton',
+    author='Nicola Sella',
+    author_email='nsella@redhat.com',
+    url='https://github.com/inknos/thinbox_py',
+    scripts=[
+        "thinbox",
+    ],
+)
