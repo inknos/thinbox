6a46415799fd5764f9f02cec0516ab09310cbc7c
Nicola Sella <nsella@redhat.com>
2021-11-11 17:28:18 +0100

    Add Env class

--- thinbox/config.py
+++ thinbox/config.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import json
+import yaml
 
 from sysconfig import get_config_var
 
@@ -24,7 +26,7 @@ THINBOX = os.path.basename(sys.argv[0])
 XDG_CONFIG_HOME = os.environ.get(
     "XDG_CONFIG_HOME", os.path.expanduser("~/.config"))
 CONFIG_DIR = os.path.join(XDG_CONFIG_HOME, "thinbox")
-USER_CONFIG = os.path.join(CONFIG_DIR, "user.thinbox.yaml")
+USER_CONFIG = os.path.join(CONFIG_DIR, "config.json")
 
 # cache directory according to XDG
 XDG_CACHE_HOME = os.environ.get(
@@ -71,6 +73,7 @@ PATHS = {
     "PYTHONPATH": [PATHS_ENV["PYTHON3_SITEARCH"], PATHS_ENV["PYTHON3_SITELIB"]],
 }
 
+# RHEL CONFIG
 RHEL_BASE_URL = os.environ.get("RHEL_BASE_URL", None)
 RHEL_BASE_HASH = {
     "MD5SUM",
@@ -80,3 +83,56 @@ RHEL_BASE_HASH = {
 RHEL_TAGS = {
     "rhel8-latest"
 }
+
+# FEDORA CONFIG
+FEDORA_TAGS = {
+    "fedora-cloud-34",
+    "fedora-cloud-35",
+}
+
+FEDORA_IMAGE_URL = {
+    "https://mirror.karneval.cz/pub/linux/fedora/linux//releases/$FEDORA_TAGS/Cloud/x86_64/images/",
+}
+
+
+class Env(object):
+    def __init__(self, config=None):
+        super().__init__()
+        # make file optional
+        # if file not found write file
+        self._thinbox_config_file = USER_CONFIG
+
+        self._thinbox_base_dir = THINBOX_BASE_DIR
+        self._thinbox_image_dir = THINBOX_IMAGE_DIR
+        self._thinbox_hash_dir = THINBOX_HASH_DIR
+        # if dir not found create dir
+        self.load()
+
+    @property
+    def thinbox_config_file(self):
+        return self._thinbox_config_file
+
+    @property
+    def thinbox_base_dir(self):
+        return self._thinbox_base_dir
+
+    @property
+    def thinbox_image_dir(self):
+        return self._thinbox_image_dir
+
+    @property
+    def thinbox_hash_dir(self):
+        return self._thinbox_hash_dir
+
+    def print(self):
+        print(json.dumps(self._data, sort_keys=False, indent=4))
+
+    def load(self):
+        with open(self.thinbox_config_file) as json_data_file:
+            self._data = json.load(json_data_file)
+
+    def save(self):
+        with open(self.thinbox_config_file, "w") as outfile:
+            json.dump(self._data, outfile)
+
+
